4b712af754bcb196b12b5a988868bc54ee5aff80
ChebbahYanis <yanis.chebbah5@etu.univ-lorraine.fr>
2025-11-02 22:37:36 +0100

    Update AudioListRenderer.php
    amélioration du renderer

--- src/classes/render/AudioListRenderer.php
+++ src/classes/render/AudioListRenderer.php
@@ -6,16 +6,47 @@ use iutnc\deefy\audio\lists\AudioList;
 class AudioListRenderer implements Renderer {
     protected AudioList $list;
 
+    // Chemin Absolu Confirmé : /dev_php/DEEFY-main/DEEFY-main/audio/
+    private const AUDIO_BASE_PATH = '/dev_php/DEEFY-main/DEEFY-main/audio/'; 
+
     public function __construct(AudioList $list) {
         $this->list = $list;
     }
 
     public function render(): string {
         $html = "<h2>{$this->list->nom}</h2><ul>";
+        
         foreach ($this->list->pistes as $piste) {
-            $html .= "<li>{$piste->titre} ({$piste->duree} sec)</li>";
+            
+            // Le filename est maintenant propre (ex: "chanson.mp3")
+            $filename = $piste->filename ?? '';
+            
+            // Construction du chemin URL : Chemin Absolu + nom de fichier
+            $source_url = self::AUDIO_BASE_PATH . $filename; 
+            
+            $filename_exists = !empty($filename); 
+            
+            $html .= "<li>";
+            $html .= "<h3>{$piste->titre}</h3>";
+            $html .= "<p>Durée : {$piste->duree} sec</p>";
+            
+            // Si le nom de fichier n'est pas vide, on affiche le lecteur audio
+            if ($filename_exists) {
+                $html .= <<<AUDIO
+                    <audio controls preload="none">
+                        <source src="{$source_url}" type="audio/mpeg">
+                        Votre navigateur ne supporte pas l'élément audio.
+                    </audio>
+                AUDIO;
+            } else {
+                $html .= "<p style='color: red;'>Source audio manquante.</p>";
+            }
+
+            $html .= "</li>";
         }
-        $html .= "</ul><p>Total : {$this->list->nb_pistes} pistes, {$this->list->duree_totale} sec</p>";
+        
+        $html .= "</ul><p>Total : **{$this->list->nb_pistes} pistes**, {$this->list->duree_totale} sec</p>";
+        
         return $html;
     }
 }
