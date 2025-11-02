e06ee490c8129c8c3b66e8fb10409f7dd8578ad0
Jassem <jassem.tamourgh9@etu.univ-lorraine.fr>
2025-11-03 00:01:17 +0100

    Implement dynamic property setting in PodcastTrack
    Added magic __set method to handle dynamic property assignments with validation.

--- src/classes/audio/tracks/PodcastTrack.php
+++ src/classes/audio/tracks/PodcastTrack.php
@@ -1,5 +1,7 @@
 <?php
 namespace iutnc\deefy\audio\tracks;
+use iutnc\deefy\exception\InvalidPropertyNameException;
+use iutnc\deefy\exception\InvalidPropertyValueException;
 
 class PodcastTrack extends AudioTrack {
     protected string $auteur;
@@ -9,5 +11,32 @@ class PodcastTrack extends AudioTrack {
         parent::__construct($titre, $filename);
         $this->auteur = "";
         $this->date = "";
+    }
+
+     public function __set(string $name, $value): void {
+        if (!property_exists($this, $name)) {
+            throw new InvalidPropertyNameException("invalid property : $name");
+        }
+
+        switch ($name) {
+            case 'duree':
+                $v = (int)$value;
+                if ($v < 0) {
+                    throw new InvalidPropertyValueException("duree doit être >= 0");
+                }
+                $this->duree = $v;
+                break;
+
+            case 'titre':
+            case 'genre':
+            case 'auteur':
+            case 'date':
+            case 'filename':
+                $this->$name = (string)$value;
+                break;
+
+            default:
+                throw new InvalidPropertyNameException("invalid property : {$name}");
+        }
     }
 }
